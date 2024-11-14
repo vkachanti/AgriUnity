500de9bde437a77a92411578f91480b307b832b5
Achanti Vamsi Krishna <151735259+vkachanti@users.noreply.github.com>
2024-11-14 07:05:26 +0530

    Update login.js

--- login.js
+++ login.js
@@ -18,7 +18,8 @@ function login() {
     const password = document.getElementById('password').value;
 
     // Validate credentials
-    if (email === "vkachanti@gmail.com" && password === "vamsi"|| email === "nagamanimandangi44@gmail.com" && password==="Naga@1445" || email === "djaveedbasha7@gmail.com" && password==="basha12345"||email === "karamrajesh10072000@gmail.com" && password==="r@jesh12345" || email === "satwikpagolu@gmail.com" && password === "materials") {
+    if (email === "vkachanti@gmail.com" && password === "vamsi"|| email === "nagamanimandangi44@gmail.com" && password==="Naga@1445" || email === "djaveedbasha7@gmail.com" && password==="basha12345"||email === "karamrajesh10072000@gmail.com" && password==="r@jesh12345" || email === "satwikpagolu@gmail.com" && password === "materials" ||
+         email === "narasimhaaditya.1424@gmail.com" && password === "aditya1424") {
         // Hide the login section
         document.getElementById("login-section").style.display = "none";
         
