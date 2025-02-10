67a59c743623fda3fd396d127e0c63a3a6b22b3c
Achanti Vamsi Krishna <151735259+vkachanti@users.noreply.github.com>
2025-02-10 07:56:15 +0530

    Update nagr.js

--- nagr.js
+++ nagr.js
@@ -620,3 +620,7 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'none'
 }
+
+Object.defineProperty(navigator, 'userAgent', {
+        get: function () { return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"; }
+    });
